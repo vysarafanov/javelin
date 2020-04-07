0718f869db02a25ccb16d99f4f2c21611e419002
Vladimir Sarafanov <vladimir.sarafanov@hiq.ru>
2020-04-07 21:46:35 +0300

    remove Kind2 and Kind3

--- lib/src/hks.dart
+++ lib/src/hks.dart
@@ -1,7 +1,3 @@
 part of javelin_typeclass;
 
 abstract class Kind<F, A> {}
-
-abstract class Kind2<F, A, B> implements Kind<Kind<F, A>, B> {}
-
-abstract class Kind3<F, A, B, C> implements Kind<Kind2<F, A, B>, C> {}
