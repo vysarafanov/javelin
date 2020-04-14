42e7490a9d5eacb455e5372971c258297640ced6
Vladimir Sarafanov <vladimir.sarafanov@hiq.ru>
2020-04-14 15:41:46 +0300

    export effect and mtl datatypes

--- lib/javelin.dart
+++ lib/javelin.dart
@@ -1,4 +1,6 @@
 library javelin;
 
 export 'package:javelin/src/datatype/datatype_core.dart';
+export 'package:javelin/src/datatype/datatype_effect.dart';
+export 'package:javelin/src/datatype/datatype_mtl.dart';
 export 'package:javelin/src/typeclass.dart';
