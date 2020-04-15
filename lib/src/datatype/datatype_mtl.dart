cdfbf550a1159058b882fa3080c0457aa90c67c1
Vladimir Sarafanov <vladimir.sarafanov@hiq.ru>
2020-04-15 10:02:39 +0300

    add missed import

--- lib/src/datatype/datatype_mtl.dart
+++ lib/src/datatype/datatype_mtl.dart
@@ -1,6 +1,7 @@
 library datatype_mtl;
 
 import 'package:javelin/src/datatype/datatype_core.dart';
+import 'package:javelin/src/datatype/datatype_effect.dart';
 import 'package:javelin/src/datatype/mtl/extension/eithert.dart';
 import 'package:javelin/src/datatype/mtl/extension/optiont.dart';
 import 'package:javelin/src/typeclass.dart';
