c121f576221a9197d8213dc3456de94617805290
Vladimir Sarafanov <vladimir.sarafanov@hiq.ru>
2020-04-29 11:24:03 +0300

    add missed import

--- lib/src/datatype/datatype_core.dart
+++ lib/src/datatype/datatype_core.dart
@@ -1,5 +1,6 @@
 library datatype_core;
 
+import 'package:javelin/src/core.dart';
 import 'package:javelin/src/datatype/core/extension/either.dart';
 import 'package:javelin/src/datatype/core/extension/id.dart';
 import 'package:javelin/src/datatype/core/extension/option.dart';
