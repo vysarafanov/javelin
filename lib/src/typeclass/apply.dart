a7e543d455dfcbe7d8cce23b3e7b3ba6da229210
Vladimir Sarafanov <vladimir.sarafanov@hiq.ru>
2020-04-29 16:49:27 +0300

    add types for map method explicitly

--- lib/src/typeclass/apply.dart
+++ lib/src/typeclass/apply.dart
@@ -7,12 +7,15 @@ mixin Apply<F> on Functor<F> {
 }
 
 extension ApplyExt<F> on Apply<F> {
-  Kind<F, Tuple2<A, B>> product<A, B>(Kind<F, A> fa, Kind<F, B> fb) =>
-      ap(fb, map(fa, (A a) => (B b) => Tuple2(a, b)));
+  Kind<F, Tuple2<A, B>> product<A, B>(Kind<F, A> fa, Kind<F, B> fb) => ap(
+      fb, map<A, Tuple2<A, B> Function(B)>(fa, (A a) => (B b) => Tuple2(a, b)));
 
   Kind<F, Tuple3<A, B, C>> product2<A, B, C>(
           Kind<F, Tuple2<A, B>> fab, Kind<F, C> fc) =>
-      ap(fc, map(fab, (Tuple2<A, B> ab) => (C c) => Tuple3(ab.a, ab.b, c)));
+      ap(
+          fc,
+          map<Tuple2<A, B>, Tuple3<A, B, C> Function(C)>(
+              fab, (Tuple2<A, B> ab) => (C c) => Tuple3(ab.a, ab.b, c)));
 
   Kind<F, Z> map2<A, B, Z>(
           Kind<F, A> fa, Kind<F, B> fb, Z f(Tuple2<A, B> ab)) =>
